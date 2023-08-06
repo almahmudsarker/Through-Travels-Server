9cf7ef1d65292dac4085b268de1ac36310ebfcee
almahmudsarker <almahmuds427@gmail.com>
2023-08-06 20:38:21 +0600

    delete bookings & update status route added

--- index.js
+++ index.js
@@ -98,6 +98,14 @@ async function run() {
       res.send(result)
     })
 
+    //delete a booking
+    app.delete('/bookings/:id', async(req, res) => {
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) }
+      const result = await bookingsCollection.deleteOne(query)
+      res.send(result)
+    })
+
     // get user
     app.get('/users/:email', async(req, res) => {
       const email = req.params.email
